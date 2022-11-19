fc3e1af348c429f3eb145b6c179148eb9faec7aa
ringsaturn <ringsaturn.me@gmail.com>
2022-11-19 16:04:01 +0800

    setup release

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-
-pub fn load_full() -> Vec<u8> {
-    include_bytes!("../combined-with-oceans.pb").to_vec()
-}
+// pub fn load_full() -> Vec<u8> {
+//     include_bytes!("../combined-with-oceans.pb").to_vec()
+// }
 
 pub fn load_reduced() -> Vec<u8> {
     include_bytes!("../combined-with-oceans.reduce.pb").to_vec()
