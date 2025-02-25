0a800ac57e9a2785a8164b8f573f1e48a04206c4
ringsaturn <ringsaturn.me@gmail.com>
2025-02-25 22:45:08 +0800

    rename pb to bin

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 // pub fn load_full() -> Vec<u8> {
-//     include_bytes!("../combined-with-oceans.pb").to_vec()
+//     include_bytes!("../combined-with-oceans.bin").to_vec()
 // }
 
 pub fn load_reduced() -> Vec<u8> {
-    include_bytes!("../combined-with-oceans.reduce.pb").to_vec()
+    include_bytes!("../combined-with-oceans.reduce.bin").to_vec()
 }
 
 pub fn load_compressed() -> Vec<u8> {
-    include_bytes!("../combined-with-oceans.reduce.compress.pb").to_vec()
+    include_bytes!("../combined-with-oceans.reduce.compress.bin").to_vec()
 }
 
 pub fn load_preindex() -> Vec<u8> {
-    include_bytes!("../combined-with-oceans.reduce.preindex.pb").to_vec()
+    include_bytes!("../combined-with-oceans.reduce.preindex.bin").to_vec()
 }
